166429e59a5ec33474cf0534601e8721dcff26a9
Koray06 <koray.oke@hotmail.com>
2022-06-14 22:54:41 +0300

    palindromSayilar

---
+++ src/palindrom/palindromSayilar.java
@@ -0,0 +1,37 @@
+package palindrom;
+
+public class palindromSayilar {
+
+	
+	static boolean isPalindrom(int number ) {
+		int temp = number, reverseNumber= 0, lastNumber ;
+		while (temp != 0) {
+			System.out.println("--------");
+			System.out.println("sayi => " + temp);
+			
+			lastNumber = temp % 10 ;
+			System.out.println("son basamak => " + lastNumber) ;
+			
+			reverseNumber = (reverseNumber * 10 ) +lastNumber ;
+			System.out.println("yeni sayi => " + reverseNumber) ;
+			
+			temp/= 10;
+			System.out.println("yeni temp => " + temp) ;
+		}
+		
+		
+		return true;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// Palindrom Sayilar iki tarafftan okunuşu ile aynı olan sayilar
+		
+		isPalindrom(2476);
+		
+		
+
+	}
+
+}
